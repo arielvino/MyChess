46126223530e6e00dea165541936127093096d64
arielvino <arielvinog@gmail.com>
2023-07-21 10:50:25 +0300

    Started!

---
+++ app/src/main/java/com/arielvinograd/mychess/ModelCell.java
@@ -0,0 +1,18 @@
+package com.arielvinograd.mychess;
+
+public class ModelCell {
+
+
+    Piece piece;
+    public Piece getPiece(){
+        return piece;
+    }
+    protected void setPiece(Piece newPiece){
+        piece = newPiece;
+    }
+
+    //Events:
+
+    //clicked on event:
+
+}
---
+++ app/src/main/java/com/arielvinograd/mychess/Piece.java
@@ -0,0 +1,5 @@
+package com.arielvinograd.mychess;
+
+public abstract class Piece {
+    ModelCell currentLocation;
+}
